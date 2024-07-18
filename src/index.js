bfcb0e122981c1524f3d5e34468faaa41c4794bb
iamjoker021 <iamjoker021@gmail.com>
2024-07-18 14:34:39 +0530

    Add Fetch Weather API function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './style.css'
+
+const fetchWeatherForcast = (latitude, longitude) => {
+    if (parseFloat(latitude) && parseFloat(longitude)) {
+        latitude = parseFloat(latitude);
+        longitude = parseFloat(longitude);
+    }
+    else {
+        return false;
+    }
+    
+    const weather_url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m&hourly=temperature_2m&timezone=auto&forecast_days=1`
+    fetch(weather_url, {
+        mode: 'cors'
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+        const currentTime = data.current.time
+        const hourlData = data.hourly.time.map((t, i) => [t, data.hourly.temperature_2m[i]])
+        console.log(data, currentTime, hourlData);
+    }).catch((error) => {
+        console.log(error);
+    })
+}
+
+Window.fetchWeatherForcast = fetchWeatherForcast;
